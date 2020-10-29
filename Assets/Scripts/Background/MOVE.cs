0552eae0b5ef6448173b192a3642948b2ab0ec89
Mert Can Türkyılmaz <turkyilmazmertcan@gmail.com>
2020-10-29 20:33:42 +0300

    Initial commit

---
+++ Assets/Scripts/Background/MOVE.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MOVE : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private float cameray;
+    private float Height;
+
+    void Start()
+    {
+        cameray = Camera.main.gameObject.transform.position.y - 15f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(Height);
+        Move();
+        Position();
+    }
+    void Move()
+    {
+        Vector3 temp = transform.position;
+        temp.y -= speed * Time.deltaTime;
+        transform.position = temp;
+    }
+
+    void Position()
+    {
+        if (transform.position.y < cameray)
+        {
+          
+            Vector3 temp = transform.position;
+            temp.y = 25f;
+            transform.position = temp;
+            
+        }
+    }
+}
---
+++ Assets/Scripts/New Folder/buttons.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttons : MonoBehaviour
+{
+    float speed = 3f;
+    void Start()
+    {
+      
+    }
+ 
+    void Update()
+    {
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed, Space.Self); //LEFT
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * speed, Space.Self); //RIGHT
+        }
+        
+       
+    }
+    public void OnLeft()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime * speed, Space.Self); //LEFT
+    }
+    public void OnRight()
+    {
+
+        transform.Translate(Vector3.down * Time.deltaTime * speed, Space.Self); //RIGHT
+    }
+}
