6b7110a96bf648cc0ccef94e584cb429011a79c0
Şerife <serifeg.99@gmail.com>
2021-01-10 20:55:55 +0300

    Sahneler tamamlandı

---
+++ Assets/gamebuttons.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gamebuttons : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+
+    public void goStore()
+    {
+        SceneManager.LoadScene(2);
+
+    }
+
+    public void backtostp()
+    {
+        SceneManager.LoadScene(3);
+
+    }
+
+    public void exitbtn()
+    {
+        SceneManager.LoadScene(0);
+
+    }
+
+}
